6cbaa934b82b5c81414859270cfded2bde6d6b04
raina <raina@rainascherer.com>
2022-02-18 11:42:07 -0800

    Added color classification and legend to map

--- visualization-1.R
+++ visualization-1.R
@@ -17,17 +17,37 @@ locations_by_frequency <- df_viz %>%
   mutate(radius = ((n / max(n)) * 3) ^ 2) %>%
   arrange(desc(radius))
 
+# Convert Long and Lat to numeric values
 locations_by_frequency$Latitude <- as.numeric(locations_by_frequency$Latitude)
 locations_by_frequency$Longitude <- as.numeric(locations_by_frequency$Longitude)
 
+# Add column to classify oceans
+locations_by_frequency <- locations_by_frequency %>%
+  mutate(
+    ocean_category = ifelse(Longitude > -100, "Atlantic", ifelse(Latitude < 66.5, "Pacific","Arctic"))
+  )
+
+# Create color palette based on ocean classification
+palette_fn <- colorFactor(palette = "Set1", domain = locations_by_frequency$ocean_category)
+
+# Create map of collection locations with increasing radius for samples taken
+# and color coding for each ocean classification
 collection_map <- leaflet(locations_by_frequency) %>%
-  addTiles() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(
     lat = ~Latitude,
     lng = ~Longitude,
     radius = ~radius,
     stroke = FALSE,
-    fillOpacity = 0.5
+    fillOpacity = 0.5,
+    color = ~palette_fn(ocean_category)
+  ) %>%
+  addLegend(
+    position = "bottomright",
+    title = "Oceans Sampled",
+    pal = palette_fn, 
+    values = ~ocean_category, 
+    opacity = 1
   )
 
 collection_map
