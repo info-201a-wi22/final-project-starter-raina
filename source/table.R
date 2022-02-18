ab89036f6ce77e5808bf86a36feccbda1e8b0834
raina <raina@rainascherer.com>
2022-02-18 12:29:55 -0800

    Fix table column names

--- source/table.R
+++ source/table.R
@@ -1,6 +1,6 @@
 library("dplyr")
 
-df <- read.csv("./data/CODAP_NA_v2021.csv",stringsAsFactors = FALSE)
+df <- read.csv("./data/CODAP_NA_v2021.csv", stringsAsFactors = FALSE)
 
 df$pH_TS_insitu_calculated <- as.numeric(df$pH_TS_insitu_calculated)
 df$Latitude <- as.numeric(df$Latitude)
@@ -13,11 +13,11 @@ acidification_table <- df %>%
   mutate(
     ocean_category = ifelse(Longitude > -100, "Atlantic", ifelse(Latitude < 66.5, "Pacific","Arctic"))
   ) %>%
-  group_by(ocean_category,Year_UTC) %>%
+  group_by(ocean_category, Year_UTC) %>%
   summarise(
-    average_pH = round(mean(pH_TS_insitu_calculated),2)
+    average_pH = round(mean(pH_TS_insitu_calculated), 2)
     ) %>%
   spread(key = ocean_category, value = average_pH) %>%
-  rename(Year = Year_UTC)
+  rename(Year = Year_UTC, "Avg. Arctic pH" = Arctic, "Avg. Atlantic pH" = Atlantic, "Avg. Pacific pH" = Pacific)
 
 acidification_table
