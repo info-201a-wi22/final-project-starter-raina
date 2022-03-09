b2a4b5527e60436e1146c91ba19a267716cdb60a
raina <raina@rainascherer.com>
2022-03-08 18:12:30 -0800

    Moved final project files for shinyapp compatibility

---
+++ source/ShinyAppFiles/app.R
@@ -0,0 +1,24 @@
+# App File
+
+# Source Files
+source("app_ui.R", local = TRUE)
+source('app_server.R', local = TRUE)
+source("interactive_map.R", local = TRUE)
+source("interactive_pH_and_co2_time.R", local = TRUE)
+
+# Libraries
+library("shiny")
+library("dplyr")
+library("leaflet")
+library(tidyverse)
+library(plotly)
+library(zoo)
+
+# Source UI File
+ui <- multipage_ui
+
+# Source Server File
+remote_server <- project_server
+
+# Run the application 
+shinyApp(ui = ui, server = remote_server)
---
+++ source/ShinyAppFiles/app_ui.R
@@ -0,0 +1,128 @@
+# UI Code
+library("shiny")
+library("dplyr")
+library("leaflet")
+library(tidyverse)
+library(plotly)
+library(zoo)
+library("bslib")
+
+# Introduction: Caroline doing text content
+introduction_page <- tabPanel(
+  "Introduction",
+  titlePanel("Introduction"),
+  p("Introduction content")
+)
+
+# Interactive Page 1: pH Over Time (Zach)
+ph_visualization <- tabPanel(
+  "Q1. CO2 and pH Over Time",
+  tabPanel(
+    "CO2 and pH Over Time Graph",
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons("co2_or_ph", 
+                     label = "Select CO2 or Average pH level",
+                     choices = c("Average pH", "CO2 fugacity"),
+                     selected = "Average pH")
+      ),
+      mainPanel(
+        h3("How have pH levels changed in oceans from 2005 to 2015?"),
+        plotlyOutput("co2_ph_plot"),
+        p(strong("Description")),
+        br("This graph aims to show the correlation between the dissolved CO2 in ocean water and the average pH 
+           level. We can clearly see that as the fugacity of CO2 increases, the pH level of ocean water decreases 
+           becoming more acidic. A possible cause of this is that more CO2 is being released into the atmosphere due 
+           to human-caused pollution. This excess CO2 could then dissolve into the ocean which will then cause the 
+           ocean to become more acidic.")
+      )
+    )
+  )
+)
+
+# Interactive Page 2: TBD (Ben)
+visualization_1 <- tabPanel(
+  "Data Visualization 1 Placeholder", 
+
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "var_select",
+        label = "placeholder",
+        choices = c("A", "B", "C")
+      )
+    ),
+    mainPanel(
+      h3("How will the changes in calcite levels impact marine ecosystems?"),
+      p("Placeholder"),
+      h2("Takeaways")
+    )
+  )
+)
+
+# Interactive Page 3: map (Raina)
+map_visualization <- tabPanel(
+  "Q3. Map Visualization", 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "year_select",
+        label = "Observation Year",
+        choices = c(2007:2017),
+        selected = 2013
+      ),
+      p("Only 2012 and 2013 contain observations for all three sampled oceans.")
+    ),
+    mainPanel(
+      h3("Which areas around the North American coastline are most affected by
+         ocean acidification?"),
+      leafletOutput(outputId = "interactive_map"),
+      br(" "),
+      h4("Average pH Level for Selected Year"),
+      tableOutput(outputId = "grouped_table"),
+      br(" "),
+      h4("Takeaways"),
+      p("This page shows both a visual and tabular representation of the pH
+        levels of each Ocean, per year. The table has this information averaged
+        for each year, whereas the map shows each individual measurement, scaled 
+        larger the more acidic the sample was."),
+      p("All oceans have become more acidic over the length of the data set,
+        however it seems as though Arctic ocean has overall suffered the
+        worst acidification. From 2010 to 2013, the average pH went from 8.21
+        to 8.14. The numbers may seem relatively small, but as the National Oceanic and
+        Atmospheric Administration explains, “the pH scale is logarithmic, so
+        [a 0.1] change represents approximately a 30 percent increase in acidity”
+        (NOAA, 2020). In other words, these small changes have massive
+        ramifications for ocean life.")
+    )
+  )
+)
+
+# Summary: Caroline doing text content
+summary_page <- tabPanel(
+  "Summary Takeaways",
+  titlePanel("Summary"),
+  p("Summary content goes here")
+)
+
+# Report: Caroline doing text content (??)
+report_page <- tabPanel(
+  "Report",
+  titlePanel("Final Report"),
+  p("Report Content Goes Here")
+)
+
+multipage_ui <- navbarPage(
+  theme = bs_theme(
+    bootswatch = "flatly",
+      ),
+  "The Impacts of Ocean Acidification", 
+  introduction_page, 
+  ph_visualization,
+  visualization_1,
+  map_visualization,
+  summary_page,
+  report_page
+)
+
+multipage_ui
